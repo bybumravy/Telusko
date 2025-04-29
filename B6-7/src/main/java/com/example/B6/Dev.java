0ac4265d41d5e7a4a59370f19a0acaf998b3a7e5
PhuongAnh <helioszebric13295@gmail.com>
2025-04-30 02:33:24 +0700

    B6-7

---
+++ B6-7/src/main/java/com/example/B6/Dev.java
@@ -0,0 +1,26 @@
+package com.example.B6;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Dev {
+    @Autowired
+    @Qualifier("laptop")
+    private Computer comp;
+
+//    public Dev(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
+//    @Autowired
+//    public void setLaptop(Laptop laptop) {
+//        this.laptop = laptop;
+//    }
+
+    public void build(){
+        comp.compile();
+        System.out.println("Working on awesome project");
+    }
+}
---
+++ B6-7/src/test/java/com/example/B6/B6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.B6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
