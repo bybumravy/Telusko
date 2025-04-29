68a1765f8e7b9b15309e08f068d5a575315bf5d3
bybumravy <tnhavy97.9@gmail.com>
2025-04-29 22:08:56 +0700

    Bai7

---
+++ Bai7/src/test/java/com/Test/Bai7/Bai7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Test.Bai7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bai7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
