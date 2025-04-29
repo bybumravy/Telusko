65900159d2040ca7a135517094742a9aed299c6e
bybumravy <tnhavy97.9@gmail.com>
2025-04-29 13:02:00 +0700

    Change

---
+++ Lesson5/src/main/java/com/telusko/Lesson5/Hello.java
@@ -0,0 +1,12 @@
+package com.telusko.Lesson5;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @RequestMapping("/")
+    public String greet() {
+        return "Hello World";
+    }
+}
