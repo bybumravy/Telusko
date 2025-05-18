64792872cb70b658f0a18c1ee4531933f4af4bbb
PhuongAnh <helioszebric13295@gmail.com>
2025-05-18 10:49:38 +0700

    B21-22

---
+++ src/main/java/com/example/B21_22/B2122Application.java
@@ -0,0 +1,13 @@
+package com.example.B21_22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2122Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2122Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/B21_22/B2122ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.B21_22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2122ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
