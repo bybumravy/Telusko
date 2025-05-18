a7381e3e2905af9b55bf8df38014e529a52125bd
PhuongAnh <helioszebric13295@gmail.com>
2025-05-18 11:50:10 +0700

    FrondEnd

---
+++ FrondEnd/stopWatch/src/App.jsx
@@ -0,0 +1,7 @@
+import StopWatch from './StopWatch.jsx'
+
+function App() {
+  return(<StopWatch/>)
+}
+
+export default App
---
+++ FrondEnd/useEffect/useEffect/src/MyComponent.jsx
@@ -0,0 +1,48 @@
+// useEffect() = React Hook that tells React DO SOME CODE WHEN (pick one):
+// 		This component re-renders
+// 		This component mounts
+// 		The state of a value
+// useEffect(function, [dependencies])
+// 1. useEffect(() =>{})            // Runs after every re-render
+// 2. useEffect(() => {}, [])       // Runs only on mount
+// 3. useEffect(() => {}, [value])  // Runs on mount + when value changes
+
+// + USES
+// #1 Event Listeners
+// #2 DOM manipulation
+// #3 Subscriptions (real-time updates)
+// #4 Fetching Data from an API
+// #5 Clean up when a component unmounts
+
+import React, {useState, useEffect} from "react";
+
+function MyComponent(){
+
+    const [count, setCount] = useState(0);
+    const [color, setColor] = useState("green");
+
+    // Keepj code more organized
+    useEffect(() => {
+        document.title = `Count: ${count} ${color}`;
+    }, [count, color]);  //[] sẽ khiến title không change mỗi khi click nữa
+
+    function addCount (){
+        setCount(c => c +1);
+    }
+
+    function subtractCount (){
+        setCount(c => c - 1);
+    }
+    function changeColor (){
+        setColor(c => c === "green" ? "red" : "green")
+    }
+
+    return (<>
+        <p style={{color: color}}>Count: {count}</p>
+        <button onClick={addCount}>Add</button>
+        <button onClick={subtractCount}>Subtract</button><br/>
+        <button onClick={changeColor}>Change Color</button><br/>
+
+        </>);
+}
+export default MyComponent
