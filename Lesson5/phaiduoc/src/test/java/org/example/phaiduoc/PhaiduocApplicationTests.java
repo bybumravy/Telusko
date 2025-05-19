5b2a603fca709abf5797cceb7243663128ec2e64
KhanhLinh57 <pklcva1332004@gmail.com>
2025-05-19 16:38:49 +0700

    Demo

---
+++ Lesson5/phaiduoc/src/test/java/org/example/phaiduoc/PhaiduocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.phaiduoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhaiduocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
